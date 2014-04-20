bc7e22f2de5929f2a80de8ab472b20587f7d7c98
Mvhls <mvernhein@gmail.com>
2014-04-20 17:24:34 -0500

    Update and rename my_solution.js to gc1_pseudocode.js

---
+++ week_3/5_group_project/gc1_pseudocode.js
@@ -0,0 +1,25 @@
+//----- PSEUDOCODE -----
+
+// Addition function:
+//------------------
+// create a function called sum
+// initialize total to zero
+// for every index in array
+// add to total
+// return the total
+
+// Median function:
+//-------------------
+// create a function called median
+// initialize total to zero
+// for every number in array
+// add to total
+// return the total divided by the array length
+
+// Mean function:
+//--------------------
+// create a function called mean
+// if the mean has an even length
+// return the median number in an even array
+// else
+// return the median number in an odd array
