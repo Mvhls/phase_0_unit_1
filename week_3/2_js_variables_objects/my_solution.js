3b0e15b7657a5db8148245d2a380a81e8b1e55df
Mvhls <mvernhein@gmail.com>
2014-04-20 22:06:46 -0500

    Update my_solution.js

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -28,13 +28,14 @@ var members = ["John", "Matthew", "Jeff", "Mary"];
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
+/*
+This challenge was pretty straight forward. I spent a lot of time reading the similarities between
+javascript and ruby. Ruby being the first language I was familiar with, javascript seemed right at home.
+Branching and looping are very similar. This challenge was also very helpful with learning to pseudo code,
+and after I had my ideas written down, writing the actual code was easy. I started with sublime text writing
+the pseudo code and the first version of my code. I transferred everything to chrome dev tools to figure out
+if they passed the tests. This exercise was straight forward, so I did not have any problems passing the tests.
+*/
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
